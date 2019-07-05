5f19955bed4b0181a496b9d3198796a219e2214c
Samer Dabra <samer.dabra@googlemail.com>
2019-07-05 12:22:46 +0100

    Quick push for Session 2

---
+++ frontend/src/components/WidgetBuilder.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components"
+
+import { Heading } from "./styles"
+
+const WidgetBuilder = () => {
+    return <>
+    <Heading h2>Did this X spark joy?</Heading>
+    </>
+}
+
+export default WidgetBuilder
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -6,11 +6,14 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import { CentralColumn } from "../components/styles"
 
+import WidgetBuilder from "../components/WidgetBuilder"
+
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO/>
     <CentralColumn>
-    <p>this is body</p>
+      <p>Ask if it sparked joy.</p>
+      <WidgetBuilder />
     </CentralColumn>
   </Layout>
 )
