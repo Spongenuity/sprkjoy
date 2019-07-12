5fda93e27de3b7c2295694a043a22a4a68f851f4
samdab02 <samer.dabra2@arm.com>
2019-07-12 02:59:25 +0100

    GraphQl mutations for saving widgets to DB - Session 5 Complete

--- backend/src/graphql.ts
+++ backend/src/graphql.ts
@@ -1,6 +1,14 @@
 import { ApolloServer, gql } from "apollo-server-lambda"
+import AWS from "aws-sdk"
+import uuidv4 from "uuid/v4"
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+
     type Widget {
         widgetId: String!
         name: String
@@ -16,6 +24,32 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
         hello:() => "Hello World"
+    },
+    Mutation: {
+        saveWidget: async ( a: any, { name }: { name: string}) => {
+            const widgetId = uuidv4();
+
+            const result = await new Promise((resolve, reject) => {
+                dynamoDB.update({
+                    TableName: process.env.DYNAMOTABLE!,
+                    Key: { widgetId },
+                    UpdateExpression: "SET widgetId = :widgetId name = :name",
+                    ExpressionAttributeValues: {
+                        ":widgetId": widgetId,
+                        ":name": name
+                    }
+                });
+            });
+
+            console.log(result)
+            return {
+                name,
+                widgetId,
+                thumbsup: 0,
+                thumbsdown: 0
+
+            }
+        }
     }
 };
 
