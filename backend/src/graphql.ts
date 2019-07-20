ba872ab30286bfc5af4d103928ac58c36a72c2ca
samdab02 <samer.dabra2@arm.com>
2019-07-20 15:49:38 +0100

    Render one field at at time

--- backend/src/graphql.ts
+++ backend/src/graphql.ts
@@ -3,128 +3,152 @@ import uuidv4 from "uuid/v4";
 import { updateItem, getItem, scanItems } from "./dynamodb";
 
 const typeDefs = gql`
-	type Widget {
-		name: String!
-		widgetId: String!
-		thumbsup: Int
-		thumbsdown: Int
-		followupQuestions: String
-	}
-	type Query {
-		widget(widgetId: String!): Widget
-		allWidget: [Widget]
-	}
-	type Mutation {
-		saveWidget(
-			name: String!
-			widgetId: String
-			followupQuestions: String
-		): Widget
-		widgetVote(
-			widgetId: String!
-			thumbsup: Boolean
-			thumbsdown: Boolean
-		): Widget
-	}
+    type Widget {
+        name: String!
+        widgetId: String!
+        thumbsup: Int
+        thumbsdown: Int
+        followupQuestions: String
+    }
+
+    type Feedback {
+        widgetId: String!
+        values: String!
+    }
+
+    type Query {
+        widget(widgetId: String!): Widget
+        allWidget: [Widget]
+    }
+
+    type Mutation {
+        saveWidget(
+            name: String!
+            widgetId: String
+            followupQuestions: String
+        ): Widget
+        widgetVote(
+            widgetId: String!
+            thumbsup: Boolean
+            thumbsdown: Boolean
+        ): Widget
+        saveFeedback(widgetId: String!, values: String!): Feedback
+    }
 `;
 
 const resolvers = {
-	Query: {
-		widget: async (_: any, { widgetId }: { widgetId: string }) => {
-			const result = await getItem({ Key: { widgetId } });
+    Query: {
+        widget: async (_: any, { widgetId }: { widgetId: string }) => {
+            const result = await getItem({ Key: { widgetId } });
 
-			if (!result.Item) {
-				return {};
-			}
+            if (!result.Item) {
+                return {};
+            }
 
-			return {
-				...result.Item,
-				name: result.Item.widgetName
-			};
-		},
-		allWidget: async () => {
-			const result = await scanItems({});
+            return {
+                ...result.Item,
+                name: result.Item.widgetName
+            };
+        },
+        allWidget: async () => {
+            const result = await scanItems({});
 
-			if (!result.Items) {
-				return [];
-			}
+            if (!result.Items) {
+                return [];
+            }
 
-			return result.Items.map(widget => ({
-				...widget,
-				name: widget.widgetName
-			}));
-		}
-	},
-	Mutation: {
-		saveWidget: async (
-			_: any,
-			{
-				name,
-				widgetId,
-				followupQuestions
-			}: { name: string; widgetId?: string; followupQuestions?: string }
-		) => {
-			if (!widgetId) {
-				widgetId = uuidv4();
-			}
+            return result.Items.map(widget => ({
+                ...widget,
+                name: widget.widgetName
+            }));
+        }
+    },
+    Mutation: {
+        saveWidget: async (
+            _: any,
+            {
+                name,
+                widgetId,
+                followupQuestions
+            }: { name: string; widgetId?: string; followupQuestions?: string }
+        ) => {
+            if (!widgetId) {
+                widgetId = uuidv4();
+            }
 
-			const result = await updateItem({
-				Key: { widgetId },
-				UpdateExpression:
-					"SET widgetName = :name, thumbsup = :thumbsup, thumbsdown = :thumbsdown, followupQuestions = :followupQuestions",
-				ExpressionAttributeValues: {
-					":name": name,
-					":followupQuestions": followupQuestions,
-					":thumbsup": 0,
-					":thumbsdown": 0
-				}
-			});
+            const result = await updateItem({
+                Key: { widgetId },
+                UpdateExpression:
+                    "SET widgetName = :name, thumbsup = :thumbsup, thumbsdown = :thumbsdown, followupQuestions = :followupQuestions",
+                ExpressionAttributeValues: {
+                    ":name": name,
+                    ":followupQuestions": followupQuestions,
+                    ":thumbsup": 0,
+                    ":thumbsdown": 0
+                }
+            });
 
-			console.log(result);
+            console.log(result);
 
-			return {
-				name,
-				widgetId,
-				followupQuestions,
-				thumbsup: 0,
-				thumbsdown: 0
-			};
-		},
-		widgetVote: async (
-			_: any,
-			{
-				widgetId,
-				thumbsup = false,
-				thumbsdown = false
-			}: { widgetId: string; thumbsup?: boolean; thumbsdown?: boolean }
-		) => {
-			const { Attributes } = await updateItem({
-				Key: { widgetId },
-				UpdateExpression:
-					"SET thumbsup = thumbsup + :thumbsup, thumbsdown = thumbsdown + :thumbsdown",
-				ExpressionAttributeValues: {
-					":thumbsup": thumbsup ? 1 : 0,
-					":thumbsdown": thumbsdown ? 1 : 0
-				},
-				ReturnValues: "ALL_NEW"
-			});
+            return {
+                name,
+                widgetId,
+                followupQuestions,
+                thumbsup: 0,
+                thumbsdown: 0
+            };
+        },
+        widgetVote: async (
+            _: any,
+            {
+                widgetId,
+                thumbsup = false,
+                thumbsdown = false
+            }: { widgetId: string; thumbsup?: boolean; thumbsdown?: boolean }
+        ) => {
+            const { Attributes } = await updateItem({
+                Key: { widgetId },
+                UpdateExpression:
+                    "SET thumbsup = thumbsup + :thumbsup, thumbsdown = thumbsdown + :thumbsdown",
+                ExpressionAttributeValues: {
+                    ":thumbsup": thumbsup ? 1 : 0,
+                    ":thumbsdown": thumbsdown ? 1 : 0
+                },
+                ReturnValues: "ALL_NEW"
+            });
 
-			return {
-				...Attributes,
-				name: Attributes && Attributes.widgetName
-			};
-		}
-	}
+            return {
+                ...Attributes,
+                name: Attributes && Attributes.widgetName
+            };
+        },
+        saveFeedback: async (
+            _: any,
+            { widgetId, values }: { widgetId: string; values: any }
+        ) => {
+            const { Attributes } = await updateItem({
+                TableName: process.env.FEEDBACKS_TABLE!,
+                Key: { widgetId },
+                UpdateExpression: "SET values = :values",
+                ExpressionAttributeValues: {
+                    ":values": values
+                },
+                ReturnValues: "ALL_NEW"
+            });
+
+            return Attributes;
+        }
+    }
 };
 
 export const server = new ApolloServer({
-	typeDefs,
-	resolvers
+    typeDefs,
+    resolvers
 });
 
 export const handler = server.createHandler({
-	cors: {
-		origin: "*",
-		credentials: true
-	}
+    cors: {
+        origin: "*",
+        credentials: true
+    }
 });
